d01e5bca8b8c34c6a9c41aeb52006c834b0f40cf
Jeffu <jeffu@jeffu.tv>
2018-03-06 12:20:23 -0500

    Unity
    add Unity project

---
+++ Assets/CubeDestroy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeDestroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < -10) {
+			Destroy (gameObject);
+		} 
+	}
+}
---
+++ Assets/CubeSpawn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeSpawn : MonoBehaviour {
+
+	public int Age = 20;
+	public string name = "Jeff";
+	public string thisClass;
+
+	public GameObject cubePrefab;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hello World");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Instantiate (cubePrefab);
+	}
+}
